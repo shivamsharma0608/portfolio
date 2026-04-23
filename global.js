84f546aa036077dd85350b8dbcc4c125da28070f
shivamsharma0608 <shivamsharma0608@gmail.com>
2026-04-23 12:59:09 -0700

    added new js files and projects.json files

--- global.js
+++ global.js
@@ -99,27 +99,31 @@ document.body.insertAdjacentHTML(
 
   export async function fetchJSON(url) {
     try {
-      // Fetch the JSON file from the given URL
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error(`Failed to fetch projects: ${response.statusText}`);}
-        const data = await response.json();
-        return data;
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
     } catch (error) {
       console.error('Error fetching or parsing JSON data:', error);
     }
   }
-
-  export function renderProjects(project, containerElement) {
-    // Your code will go here
+  
+  export function renderProjects(projects, containerElement, headingLevel = 'h2') {
     containerElement.innerHTML = '';
-    const article = document.createElement('article');
-    article.innerHTML = `
-    <h3>${project.title}</h3>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>`;
-    containerElement.appendChild(article);
+    for (let project of projects) {
+      const article = document.createElement('article');
+      article.innerHTML = `
+        <${headingLevel}>${project.title}</${headingLevel}>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>
+      `;
+      containerElement.appendChild(article);
+    }
+  }
 
-
+  export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${shivamsharma0608}`);
   }
   
--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,8 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(projects, projectsContainer, 'h2');
+
+const title = document.querySelector('.projects-title');
+title.textContent = `Projects (${projects.length})`;
